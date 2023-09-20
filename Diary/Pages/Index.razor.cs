2ed1bc2e32aed985c457230b59f29f632ad94d99
tp <815546553@qq.com>
2023-09-20 14:02:31 +0800

    Index

--- Diary/Pages/Index.razor.cs
+++ Diary/Pages/Index.razor.cs
@@ -6,4 +6,10 @@ namespace Diary.Pages;
 public partial class Index : DiariesPageComponentBase
 {
     private StringNumber tab = 0;
+
+    private Task Search(string? searchText)
+    {
+        Console.WriteLine(searchText);
+        return Task.CompletedTask;
+    }
 }
